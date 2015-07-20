02adee61ed0d3789cb850d6faa83fff4fe09466c
JohnFlabs <john_198708@mail.ru>
2015-07-20 10:58:57 +0600

    .

---
+++ rule/Workflow/Resources/scripts/form/workdocdept/PostSave.groovy
@@ -0,0 +1,15 @@
+package form.workdocdept
+
+import kz.nextbase.script._Document
+import kz.nextbase.script._Session
+import kz.nextbase.script.events._FormPostSave
+
+class PostSave extends _FormPostSave {
+
+	@Override
+	public void doPostSave(_Session ses, _Document doc) {
+	
+		
+	}
+	
+}
---
+++ rule/Workflow/Resources/scripts/form/workdocdept/QuerySave.groovy
@@ -0,0 +1,19 @@
+package form.workdocdept
+
+import kz.nextbase.script._Document
+import kz.nextbase.script._Session
+import kz.nextbase.script._WebFormData
+import kz.nextbase.script.events._FormQuerySave
+
+class QuerySave extends _FormQuerySave {
+
+
+
+	@Override
+	public void doQuerySave(_Session session, _Document doc,_WebFormData webFormData, String lang) {
+
+		
+	}
+
+	
+}
